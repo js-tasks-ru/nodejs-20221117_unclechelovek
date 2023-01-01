5ccdec273ad7799fc8f202944e562968422f25e3
Scrooge <unclechelovek@gmail.com>
2023-01-02 00:43:15 +0300

    streams 1st task

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,16 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit;
+    this.counter = 0;
   }
 
   _transform(chunk, encoding, callback) {
-
+    this.counter += chunk.length;
+    if (this.counter > this.limit) {
+      return callback(new LimitExceededError);
+    }
+    callback(null, chunk);
   }
 }
 
