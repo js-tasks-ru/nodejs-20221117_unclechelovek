7c67c32768a45b7ca353983c403e87945c4a867e
Scrooge <unclechelovek@gmail.com>
2023-01-03 07:48:23 +0300

    3rd and 4th tasks

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,17 +1,39 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
+  
+  if (pathname.includes('/') || pathname.includes('..')) {
+    res.statusCode = 400;
+    res.end('Nested paths are not allowed');
+  };
 
   const filepath = path.join(__dirname, 'files', pathname);
-
+  
   switch (req.method) {
     case 'GET':
+      const stream = fs.createReadStream(filepath);
+      stream.pipe(res);
+
+      stream.on('error', (error) => {
+        if (error.code === 'ENOENT') {
+          res.statusCode = 404;
+          res.end();
+        } else {
+          res.statusCode = 500;
+          return res.end();
+        }
+      });
+
+      req.on('aborted', () => {
+        stream.destroy();
+      });
 
       break;
 
